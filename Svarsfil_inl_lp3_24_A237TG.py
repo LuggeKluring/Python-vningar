43ce5f6d1576bfa9f41a6b9dd04e0674a37844ed
mister_rivernik <bertilssonfoto@gmail.com>
2024-02-15 08:33:03 +0100

    Lagt till tentamensfiler

---
+++ Svarsfil_inl_lp3_24_A237TG.py
@@ -0,0 +1,29 @@
+# Skriv en inledande kommentar som talar om vad programmet gör. 
+
+
+# Placera dina modulimpoter här:
+
+
+# Deluppgift 1: Funktioner från deluppgift 1 i ordning.
+# Skriv din kod här:
+
+
+# Deluppgift 2: Funktioner från deluppgift 2 i ordning.
+# Skriv din kod här:
+
+
+# Deluppgift 3: Funktioner från deluppgift 3 i ordning.
+# Skriv din kod här:
+
+
+# Deluppgift 4: Funktioner från deluppgift 4 i ordning.
+# Skriv din kod här:
+
+
+# Deluppgift 5: Funktioner från deluppgift 5 i ordning.
+# Skriv din kod här:
+
+
+# Huvudprogram med Meny från deluppgift 0. Använd menyrubriker enl. uppgiftsbeskrivningen.
+# Skriv din kod här:
+
